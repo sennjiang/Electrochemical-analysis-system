1d2061a3d562b43be2615dd0d9088581d6616928
zero <3303361569@qq.com>
2021-09-04 20:27:26 +0800

    更新了一些可能没用的东西

--- src/main/java/com/bluedot/electrochemistry/dao/base/BaseMapper.java
+++ src/main/java/com/bluedot/electrochemistry/dao/base/BaseMapper.java
@@ -135,21 +135,22 @@ public interface BaseMapper {
      * @param pageSize 一页的数据量
      * @return 被查找到的指定文件
      */
-    File findFileById(int id,int pageStart,int pageSize);
+    File getFileById(int id,int pageStart,int pageSize);
 
     /**
      * 电化学分析模块查询文件
      * @author zero
      * @param username 查询指定账户的文件
-     * @param condition 查询的关键字
+     * @param queryCondition 查询条件
+     * @param queryValue 查询结果
      * @param fileType 0 CV，1 PDV，2 SWV，3 LSV
      * @param timeOrder 0按时间降序，1按时间升序
      * @param pageStart 起始页码
      * @param pageSize 一页的数据量
      * @return 文件列表
      */
-    List<File> getFilesByCondition(int username, String condition, int fileType, int timeOrder, int pageStart, int pageSize);
-    long getFilesCountByCondition(int username, String condition, int fileType, int timeOrder);
+    List<File> getFilesByQueryCondition(int username, String queryCondition, String queryValue, int fileType, int timeOrder, int pageStart, int pageSize);
+    long getFilesCountByQueryCondition(int username, String queryCondition, int fileType, int timeOrder);
 
     /**
      * 获取所有算法集合
@@ -163,15 +164,15 @@ public interface BaseMapper {
 
     /**
      * @author zero
-     * @param condition 关键字
+     * @param queryCondition 关键字
      * @param type 算法类型 0滤波处理，1平滑处理，2 CV, 3 DPV, 4 SWV, 5 LSV
      * @param timeOrder 0按时间降序，1按时间升序
      * @param pageStart 起始页码
      * @param pageSize 一页的数据量
      * @return 算法列表
      */
-    List<Algorithm> getAlgorithmsByCondition(String condition,int type,int timeOrder,int pageStart, int pageSize);
-    long getAlgorithmsCountByCondition(String condition,int type,int timeOrder);
+    List<Algorithm> getAlgorithmsByQueryCondition(String queryCondition,int type,int timeOrder,int pageStart, int pageSize);
+    long getAlgorithmsCountByQueryCondition(String queryCondition,int type,int timeOrder);
 
     /**
      * 获取所有算法申请集合
@@ -185,13 +186,13 @@ public interface BaseMapper {
 
     /**
      * @author zero
-     * @param condition 关键字
+     * @param queryCondition 关键字
      * @param type 算法类型 0滤波处理，1平滑处理，2 CV, 3 DPV, 4 SWV, 5 LSV
      * @param timeOrder 0按时间降序，1按时间升序
      * @param pageStart 起始页码
      * @param pageSize 一页的数据量
      * @return 算法申请集合
      */
-    List<AlgorithmSend> getAlgorithmSendsByCondition(String condition,int type,int timeOrder,int pageStart, int pageSize);
-    long getAlgorithmSendsCountByCondition(String condition,int type,int timeOrder);
+    List<AlgorithmSend> getAlgorithmSendsByQueryCondition(String queryCondition,int type,int timeOrder,int pageStart, int pageSize);
+    long getAlgorithmSendsCountByQueryCondition(String queryCondition,int type,int timeOrder);
 }
