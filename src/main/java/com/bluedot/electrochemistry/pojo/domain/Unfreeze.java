3bc6778bb4b548ad2cb44afc340774e256274e72
wan-zihao <1467965694@qq.com>
2021-09-04 15:13:55 +0800

    Freeze实体类 Unfreeze实体类 Service

--- src/main/java/com/bluedot/electrochemistry/pojo/domain/Unfreeze.java
+++ src/main/java/com/bluedot/electrochemistry/pojo/domain/Unfreeze.java
@@ -6,4 +6,12 @@ package com.bluedot.electrochemistry.pojo.domain;
  * @createDate 2021/9/3-21:08
  */
 public class Unfreeze {
+    private Integer id;
+    private Integer freezeId;
+    private Integer username;
+    private String applicationTime;
+    private String email;
+    private Integer handleStatus;
+    private String refuseReason;
+    private String applicationReason;
 }
