635620584c71fde841a75a37c4631d198c4a656f
killsample <792917149@qq.com>
2021-09-04 17:08:47 +0800

    修改BaseMapper接口

--- src/main/java/com/bluedot/electrochemistry/dao/base/BaseMapper.java
+++ src/main/java/com/bluedot/electrochemistry/dao/base/BaseMapper.java
@@ -1,13 +1,10 @@
 package com.bluedot.electrochemistry.dao.base;
 
-import com.bluedot.electrochemistry.pojo.domain.Algorithm;
-import com.bluedot.electrochemistry.pojo.domain.AlgorithmSend;
-import com.bluedot.electrochemistry.pojo.domain.File;
+import com.bluedot.electrochemistry.pojo.domain.*;
 
 import java.util.List;
 
 import com.bluedot.electrochemistry.pojo.domain.File;
-import com.bluedot.electrochemistry.pojo.domain.User;
 
 import java.util.List;
 
@@ -22,7 +19,7 @@ public interface BaseMapper {
      * 获取管理员信息列表
      *
      * @param pageStart 页码
-     * @param pageSize  每页数据
+     * @param pageSize  每页大小
      * @return 管理员列表
      */
     List<User> getAdmins(int pageStart, int pageSize);
@@ -40,7 +37,7 @@ public interface BaseMapper {
      * @param queryCondition 查询条件（字段）
      * @param queryValue 要查询的值
      * @param pageStart 页码
-     * @param pageSize 每页数据
+     * @param pageSize 每页大小
      * @return 管理员列表
      */
     List<User> getAdminByQueryCondition(String queryCondition , String queryValue , int pageStart , int pageSize);
@@ -58,7 +55,7 @@ public interface BaseMapper {
      * 获取备份文件信息列表
      *
      * @param pageStart 页码
-     * @param pageSize 每页数据
+     * @param pageSize 每页大小
      * @return 备份文件列表
      */
     List<File> getBackupFiles(int pageStart , int pageSize);
@@ -76,7 +73,7 @@ public interface BaseMapper {
      * @param queryCondition 查询条件（字段）
      * @param queryValue 要查询的值
      * @param pageStart 页码
-     * @param pageSize 每页数据
+     * @param pageSize 每页大小
      * @return 备份文件列表
      */
     List<File> getFileByQueryCondition(String queryCondition , String queryValue , int pageStart , int pageSize);
@@ -89,6 +86,44 @@ public interface BaseMapper {
      * @return 备份文件数据总数
      */
     Long getFileCountByQueryCondition(String queryCondition , String queryValue);
+
+    /**
+     * 获取角色信息列表
+     *
+     * @param pageStart 页码
+     * @param pageSize 每页大小
+     * @return 角色列表
+     */
+    List<Role> getRoles(int pageStart , int pageSize);
+
+    /**
+     * 获取角色数量
+     *
+     * @return 角色数量
+     */
+    Long getRoleCount();
+
+    /**
+     * 根据查询条件获取角色信息列表
+     *
+     * @param queryCondition 查询条件（字段）
+     * @param queryValue 要查询的值
+     * @param pageStart 页码
+     * @param pageSize 每页的大小
+     * @return 角色信息列表
+     */
+    List<Role> getRoleByQueryCondition(String queryCondition , String queryValue , int pageStart , int pageSize);
+
+    /**
+     * 根据查询条件获取角色数量
+     *
+     * @param queryCondition 查询条件（字段）
+     * @param queryValue 要查询的值
+     * @return 角色数量
+     */
+    Long getRoleCountByQueryCondition(String queryCondition , String queryValue);
+
+
     List<File> listBinFileBySearch(int username, String condition, int fileType, int timeOrder, int pageStart, int pageSize);
     long getBinFileCountBySearch(int username, String condition, int fileType, int timeOrder);
 
