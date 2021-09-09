4555880c5d29869c0a0136382d7eb4811026b224
JDsen99 <64098056@qq.com>
2021-09-09 19:19:26 +0800

    更新

---
+++ src/main/java/com/bluedot/electrochemistry/service/RoleService.java
@@ -0,0 +1,11 @@
+package com.bluedot.electrochemistry.service;
+
+import com.bluedot.electrochemistry.service.base.BaseService;
+
+/**
+ * @author JDsen99
+ * @description
+ * @createDate 2021/9/9-17:55
+ */
+public class RoleService extends BaseService {
+}
