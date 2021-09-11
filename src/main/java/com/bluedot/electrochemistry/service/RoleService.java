2f42c2282f81713fb6b3de5b3ff5c1634aa7f750
killsample <792917149@qq.com>
2021-09-11 19:38:30 +0800

    修改ServiceCallback 修改角色业务类 添加管理员业务和备份文件业务

--- src/main/java/com/bluedot/electrochemistry/service/RoleService.java
+++ src/main/java/com/bluedot/electrochemistry/service/RoleService.java
@@ -1,8 +1,12 @@
 package com.bluedot.electrochemistry.service;
 
+import com.bluedot.electrochemistry.dao.base.BaseMapper;
+import com.bluedot.electrochemistry.pojo.domain.Role;
 import com.bluedot.electrochemistry.service.base.BaseService;
+import com.bluedot.electrochemistry.service.callback.ServiceCallback;
 import com.bluedot.framework.simplespring.core.annotation.Service;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,6 +16,37 @@ import java.util.Map;
  */
 @Service
 public class RoleService extends BaseService {
+
+
+    /**
+     * 查询角色信息
+     *
+     * @param map
+     */
+    private void queryRoles(Map<String , Object> map){
+        doSimpleQueryListTemplate(map, new ServiceCallback<Role>() {
+            @Override
+            public List<Role> doListExecutor(BaseMapper baseMapper, int pageStart, int pageSize) {
+                return baseMapper.getRoles(pageStart , pageSize);
+            }
+
+            @Override
+            public List<Role> doListExecutorByQueryCondition(BaseMapper baseMapper, int pageStart, int pageSize, String queryCondition, String queryValue) {
+                return baseMapper.getRoleByQueryCondition(queryCondition, queryValue, pageStart, pageSize);
+            }
+
+            @Override
+            public Long doCountExecutor(BaseMapper baseMapper) {
+                return baseMapper.getRoleCount();
+            }
+
+            @Override
+            public Long doCountExecutorByQueryCondition(BaseMapper baseMapper, String queryCondition, String queryValue) {
+                return baseMapper.getRoleCountByQueryCondition(queryCondition, queryValue);
+            }
+        });
+    }
+
     //修改角色以及该角色拥有的菜单权限
     private int modifyRole(Map map){
         return 1;
