9ea0dd9cfc929a67c02325b1ac2ddde8a629bdaf
killsample <792917149@qq.com>
2021-09-11 10:07:39 +0800

    添加UserRightView视图

--- src/main/java/com/bluedot/electrochemistry/pojo/vo/UserRightView.java
+++ src/main/java/com/bluedot/electrochemistry/pojo/vo/UserRightView.java
@@ -6,4 +6,40 @@ package com.bluedot.electrochemistry.pojo.vo;
  * @createDate 2021/9/3-21:11
  */
 public class UserRightView {
+    //用户账号
+    private Integer username;
+    //权限名称
+    private String rightName;
+
+    public UserRightView() {
+    }
+
+    public UserRightView(Integer username, String rightName) {
+        this.username = username;
+        this.rightName = rightName;
+    }
+
+    public Integer getUsername() {
+        return username;
+    }
+
+    public void setUsername(Integer username) {
+        this.username = username;
+    }
+
+    public String getRightName() {
+        return rightName;
+    }
+
+    public void setRightName(String rightName) {
+        this.rightName = rightName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRightView{" +
+                "username=" + username +
+                ", rightName='" + rightName + '\'' +
+                '}';
+    }
 }
