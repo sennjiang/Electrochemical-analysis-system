4e6f5508055795d90c8803d651fbfca7787f1944
killsample <792917149@qq.com>
2021-09-11 11:13:41 +0800

    修改RoleService和SystemService业务类

--- src/main/java/com/bluedot/electrochemistry/service/RoleService.java
+++ src/main/java/com/bluedot/electrochemistry/service/RoleService.java
@@ -3,6 +3,8 @@ package com.bluedot.electrochemistry.service;
 import com.bluedot.electrochemistry.service.base.BaseService;
 import com.bluedot.framework.simplespring.core.annotation.Service;
 
+import java.util.Map;
+
 /**
  * @author JDsen99
  * @description
@@ -10,4 +12,18 @@ import com.bluedot.framework.simplespring.core.annotation.Service;
  */
 @Service
 public class RoleService extends BaseService {
+    //修改角色以及该角色拥有的菜单权限
+    private int modifyRole(Map map){
+        return 1;
+    }
+
+    //添加角色，为角色赋予相应的权限
+    private int addRole(Map map){
+        return 1;
+    }
+
+    //删除角色
+    private int deleteRole(Map map){
+        return 1;
+    }
 }
--- src/main/java/com/bluedot/electrochemistry/service/SystemService.java
+++ src/main/java/com/bluedot/electrochemistry/service/SystemService.java
@@ -2,6 +2,9 @@ package com.bluedot.electrochemistry.service;
 
 import com.bluedot.framework.simplespring.core.annotation.Service;
 
+import java.io.File;
+import java.util.Map;
+
 /**
  * @author jun
  * @version 1.0
@@ -9,5 +12,26 @@ import com.bluedot.framework.simplespring.core.annotation.Service;
  */
 @Service
 public class SystemService {
+
+    //系统还原
+    private int systemReduction(Map map){
+        return 1;
+    }
+
+    //系统备份
+    private int systemBackup(Map map){
+        return 1;
+    }
+
+    //下载备份文件压缩包
+    private File downloadBackupFile(Map map){
+        return new File("1","1");
+    }
+
+    //删除当前的FTP
+    private int deleteFTP(Map map){
+        return 1;
+    }
+
 
 }
