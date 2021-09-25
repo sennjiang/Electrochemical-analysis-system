be97e38a692f4eb74bc7afddacfa0bb0ea9e6bfb
wan-zihao <1467965694@qq.com>
2021-09-25 17:43:33 +0800

    对模板进行了一些框架上的改动，而模板中登录验证，权限验证等有待探究

---
+++ vue-admin-template/src/views/ea/admin/algorithmManage/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>算法管理</div>
+</template>
+
+<script>
+export default {
+  name: 'AlgorithmManage'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-admin-template/src/views/ea/admin/unfreezeAudit/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>解冻审核</div>
+</template>
+
+<script>
+export default {
+  name: 'UnfreezeAudit'
+}
+</script>
+
+<style scoped>
+
+</style>
