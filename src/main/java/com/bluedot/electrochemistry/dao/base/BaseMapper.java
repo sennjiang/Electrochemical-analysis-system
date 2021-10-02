cdded861343d6cda52888ffa4231b44d09f951a5
JDsen99 <64098056@qq.com>
2021-10-02 18:34:46 +0800

    注释错误方法

--- src/main/java/com/bluedot/electrochemistry/dao/base/BaseMapper.java
+++ src/main/java/com/bluedot/electrochemistry/dao/base/BaseMapper.java
@@ -46,15 +46,6 @@ public interface BaseMapper {
      * @return User实体类
      */
     User queryUserByEmail(String email);
-
-    /**
-     * 获取管理员信息列表
-     *
-     * @param pageStart 页码
-     * @param pageSize  每页大小
-     * @return 管理员列表
-     */
-
     /**
      * 获取管理员信息列表
      *
@@ -107,16 +98,16 @@ public interface BaseMapper {
      */
     Long getBackupFileCount();
 
-    /**
-     * 根据查询条件获取备份文件信息列表
-     *
-     * @param queryCondition 查询条件（字段）
-     * @param queryValue     要查询的值
-     * @param pageStart      页码
-     * @param pageSize       每页大小
-     * @return 备份文件列表
-     */
-    List<File> getBackupFileByQueryCondition(String queryCondition, String queryValue, int pageStart, int pageSize);
+//    /**
+//     * 根据查询条件获取备份文件信息列表
+//     *
+//     * @param queryCondition 查询条件（字段）
+//     * @param queryValue     要查询的值
+//     * @param pageStart      页码
+//     * @param pageSize       每页大小
+//     * @return 备份文件列表
+//     */
+//    List<File> getBackupFileByQueryCondition(String queryCondition, String queryValue, int pageStart, int pageSize);
 
     /**
      * 根据查询条件获取备份文件数据总数
@@ -125,7 +116,7 @@ public interface BaseMapper {
      * @param queryValue     要查询的值
      * @return 备份文件数据总数
      */
-    Long getBackupFileCountByQueryCondition(String queryCondition, String queryValue);
+//    Long getBackupFileCountByQueryCondition(String queryCondition, String queryValue);
 
     /**
      * 获取角色信息列表
@@ -164,70 +155,68 @@ public interface BaseMapper {
     Long getRoleCountByQueryCondition(String queryCondition, String queryValue);
 
 
-    /**
-     * 比较hash值
-     *
-     * @param File_hash
-     * @param username
-     * @return
-     */
-    long contrastFile(String File_hash, int username);
+//    /**
+//     * 比较hash值
+//     *
+//     * @param File_hash
+//     * @param username
+//     * @return
+//     */
+//    long contrastFile(String File_hash, int username);
 
-    /**
-     * 按照条件查询
-     *
-     * @param username  账号
-     * @param condition 条件
-     * @param timeOrder 时间
-     * @param status    0 正常 1 已删除
-     * @param type      0 用户 1 系统
-     * @param pageStart 页面
-     * @param pageSize  页面大小
-     * @return List
-     */
-    List<File> listFileByQueryCondition(int username, String condition, Timestamp timeOrder,
-                                        int status, int type, int pageStart, int pageSize);
+//    /**
+//     * 按照条件查询
+//     *
+//     * @param username  账号
+//     * @param condition 条件
+//     * @param timeOrder 时间
+//     * @param status    0 正常 1 已删除
+//     * @param type      0 用户 1 系统
+//     * @param pageStart 页面
+//     * @param pageSize  页面大小
+//     * @return List
+//     */
+//    List<File> listFileByQueryCondition(int username, String condition, Timestamp timeOrder,
+//                                        int status, int type, int pageStart, int pageSize);
 
-    /**
-     * 查询查询条数
-     *
-     * @param username  账号
-     * @param condition 条件
-     * @param timeOrder 时间
-     * @param status    0 正常 1 已删除
-     * @param type      0 用户 1 系统
-     * @return List
-     */
-    long getFileCountByQuery(int username, String condition, Timestamp timeOrder,
-                             int status, int type);
+//    /**
+//     * 查询查询条数
+//     *
+//     * @param username  账号
+//     * @param condition 条件
+//     * @param timeOrder 时间
+//     * @param status    0 正常 1 已删除
+//     * @param type      0 用户 1 系统
+//     * @return List
+//     */
+//    long getFileCountByQuery(int username, String condition, Timestamp timeOrder,
+//                             int status, int type);
 
     /**
      * 根据文件id查找对应的文件
      *
      * @param id        文件的id
-     * @param pageStart 起始页码
-     * @param pageSize  一页的数据量
      * @return 被查找到的指定文件
      * @author zero
      */
-    File getFileById(int id, int pageStart, int pageSize);
+    File getFileById(int id);
 
-    /**
-     * 电化学分析模块查询文件
-     *
-     * @param username       查询指定账户的文件
-     * @param queryCondition 查询条件
-     * @param queryValue     查询结果
-     * @param fileType       0 CV，1 PDV，2 SWV，3 LSV
-     * @param timeOrder      0按时间降序，1按时间升序
-     * @param pageStart      起始页码
-     * @param pageSize       一页的数据量
-     * @return 文件列表
-     * @author zero
-     */
-    List<File> getFilesByQueryCondition(int username, String queryCondition, String queryValue, int fileType, int timeOrder, int pageStart, int pageSize);
+//    /**
+//     * 电化学分析模块查询文件
+//     *
+//     * @param username       查询指定账户的文件
+//     * @param queryCondition 查询条件
+//     * @param queryValue     查询结果
+//     * @param fileType       0 CV，1 PDV，2 SWV，3 LSV
+//     * @param timeOrder      0按时间降序，1按时间升序
+//     * @param pageStart      起始页码
+//     * @param pageSize       一页的数据量
+//     * @return 文件列表
+//     * @author zero
+//     */
+//    List<File> getFilesByQueryCondition(int username, String queryCondition, String queryValue, int fileType, int timeOrder, int pageStart, int pageSize);
 
-    long getFilesCountByQueryCondition(int username, String queryCondition, int fileType, int timeOrder);
+//    long getFilesCountByQueryCondition(int username, String queryCondition, int fileType, int timeOrder);
 
     /**
      * 获取所有算法集合
@@ -241,41 +230,43 @@ public interface BaseMapper {
 
     long getAlgorithmsCount();
 
-    /**
-     * @param queryCondition 关键字
-     * @param type           算法类型 0滤波处理，1平滑处理，2 CV, 3 DPV, 4 SWV, 5 LSV
-     * @param timeOrder      0按时间降序，1按时间升序
-     * @param pageStart      起始页码
-     * @param pageSize       一页的数据量
-     * @return 算法列表
-     * @author zero
-     */
-    List<Algorithm> getAlgorithmsByQueryCondition(String queryCondition, int type, int timeOrder, int pageStart, int pageSize);
+//    /**
+//     * @param queryCondition 关键字
+//     * @param type           算法类型 0滤波处理，1平滑处理，2 CV, 3 DPV, 4 SWV, 5 LSV
+//     * @param timeOrder      0按时间降序，1按时间升序
+//     * @param pageStart      起始页码
+//     * @param pageSize       一页的数据量
+//     * @return 算法列表
+//     * @author zero
+//     */
+//    List<Algorithm> getAlgorithmsByQueryCondition(String queryCondition, int type, int timeOrder, int pageStart, int pageSize);
 
-    long getAlgorithmsCountByQueryCondition(String queryCondition, int type, int timeOrder);
+//    long getAlgorithmsCountByQueryCondition(String queryCondition, int type, int timeOrder)、;
 
-    /**
-     * 获取所有算法申请集合
-     *
-     * @param pageStart 起始页码
-     * @param pageSize  一页的数据量
-     * @return 算法申请列表
-     * @author zero
-     */
-    List<AlgorithmSend> getAlgorithmSends(int pageStart, int pageSize);
+//    /**
+//     * 获取所有算法申请集合
+//     *
+//     * @param pageStart 起始页码
+//     * @param pageSize  一页的数据量
+//     * @return 算法申请列表
+//     * @author zero
+//     */
+//    List<AlgorithmSend> getAlgorithmSends(int pageStart, int pageSize);
 
-    long getAlgorithmSendsCount();
+//    long getAlgorithmSendsCount();
 
-    /**
-     * @param queryCondition 关键字
-     * @param type           算法类型 0滤波处理，1平滑处理，2 CV, 3 DPV, 4 SWV, 5 LSV
-     * @param timeOrder      0按时间降序，1按时间升序
-     * @param pageStart      起始页码
-     * @param pageSize       一页的数据量
-     * @return 算法申请集合
-     * @author zero
-     */
-    List<AlgorithmSend> getAlgorithmSendsByQueryCondition(String queryCondition, int type, int timeOrder, int pageStart, int pageSize);
+//    /**
+//     * @param queryCondition 关键字
+//     * @param type           算法类型 0滤波处理，1平滑处理，2 CV, 3 DPV, 4 SWV, 5 LSV
+//     * @param timeOrder      0按时间降序，1按时间升序
+//     * @param pageStart      起始页码
+//     * @param pageSize       一页的数据量
+//     * @return 算法申请集合
+//     * @author zero
+//     */
+//    List<AlgorithmSend> getAlgorithmSendsByQueryCondition(String queryCondition, int type, int timeOrder, int pageStart, int pageSize);
 
-    long getAlgorithmSendsCountByQueryCondition(String queryCondition, int type, int timeOrder);
+//    long getAlgorithmSendsCountByQueryCondition(String queryCondition, int type, int timeOrder);
+
+    List<Operation> listOperations(short type);
 }
