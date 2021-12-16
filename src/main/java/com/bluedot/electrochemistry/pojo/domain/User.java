b609b9292d5df0d01af5b2ea87f0440dc49744f1
sensen <64098056@qq.com>
2021-12-16 20:40:41 +0800

    User实体

---
+++ src/main/java/com/bluedot/electrochemistry/pojo/domain/User.java
@@ -0,0 +1,26 @@
+package com.bluedot.electrochemistry.pojo.domain;
+
+/**
+ * @author Sens
+ * @createDate 2021/12/16 19:20
+ */
+public class User {
+    private String username;
+    private Integer age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
