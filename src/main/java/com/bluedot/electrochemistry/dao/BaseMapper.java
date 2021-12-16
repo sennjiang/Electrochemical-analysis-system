24d0458237003b3266b752bc67b14b610822ea3b
sensen <64098056@qq.com>
2021-12-16 20:39:38 +0800

    增加一个测试方法

--- src/main/java/com/bluedot/electrochemistry/dao/BaseMapper.java
+++ src/main/java/com/bluedot/electrochemistry/dao/BaseMapper.java
@@ -1,4 +1,14 @@
 package com.bluedot.electrochemistry.dao;
 
+import com.bluedot.electrochemistry.pojo.domain.User;
+
+import java.util.List;
+
+/**
+ * @author Sens
+ * @createDate 2021/12/16 19:19
+ */
 public interface BaseMapper {
+
+    List<User> getList(String condition, int pageStart, int pageSize);
 }
