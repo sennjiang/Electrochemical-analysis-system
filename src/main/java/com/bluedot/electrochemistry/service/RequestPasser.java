12f5ab3ace0a28dcbf22f44d1f03bbf46049a3e3
sensen <64098056@qq.com>
2021-12-16 20:40:30 +0800

    请求传递者

---
+++ src/main/java/com/bluedot/electrochemistry/service/RequestPasser.java
@@ -0,0 +1,20 @@
+package com.bluedot.electrochemistry.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Sens
+ * @createDate 2021/12/16 20:35
+ */
+public class RequestPasser {
+
+    private int boundary;
+
+    private String username;
+
+    private HttpServletRequest request;
+
+    private Result result;
+
+
+}
---
+++ src/main/java/com/bluedot/electrochemistry/service/Result.java
@@ -0,0 +1,11 @@
+package com.bluedot.electrochemistry.service;
+
+/**
+ * @author Sens
+ * @createDate 2021/12/16 20:36
+ */
+public class Result {
+    private int codeNum;
+    private String message;
+    private Object data;
+}
