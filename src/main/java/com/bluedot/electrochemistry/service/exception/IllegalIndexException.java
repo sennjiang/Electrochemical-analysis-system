995b3b9a80d8260c8ac6fb553b2c10dc6fe9c9bf
sensen <64098056@qq.com>
2021-12-16 20:40:17 +0800

    更新非法索引异常

---
+++ src/main/java/com/bluedot/electrochemistry/service/exception/IllegalIndexException.java
@@ -0,0 +1,13 @@
+package com.bluedot.electrochemistry.service.exception;
+
+/**
+ * 非法索引异常
+ * @author Sens
+ * @createDate 2021/12/16 20:29
+ */
+public class IllegalIndexException extends Exception {
+
+    public IllegalIndexException(String msg) {
+        super(msg);
+    }
+}
